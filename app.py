67365b12b5fb41646e8c96e89e7a7055585151a9
Anandhi1509 <kpr2870@gmail.com>
2026-06-20 16:06:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def get_flow():
             "web": {
                 "client_id": st.secrets["google"]["client_id"],
                 "client_secret": st.secrets["google"]["client_secret"],
-                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                "auth_uri": "https://accounts.google.com/o/oauth2/v2/auth",
                 "token_uri": "https://oauth2.googleapis.com/token",
                 "redirect_uris": [st.secrets["google"]["redirect_uri"]],
             }
